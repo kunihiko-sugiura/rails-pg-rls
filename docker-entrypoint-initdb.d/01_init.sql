5bb8c32dbd1f44dd92c72de7130e8ec5e79c78a3
Kunihiko Sugiura <k.sugiura@n2i.jp>
2024-07-18 21:07:04 +0900

    feat: docker composeによりpostgresqlの起動

---
+++ docker-entrypoint-initdb.d/01_init.sql
@@ -0,0 +1,15 @@
+-- アプリケーション用のユーザーを作成
+CREATE ROLE app_user LOGIN PASSWORD 'passw0rd';
+CREATE ROLE operator_user LOGIN PASSWORD 'passw0rd';
+
+-- データベースの作成
+CREATE DATABASE rls_dev OWNER operator_user;
+CREATE DATABASE rls_test OWNER operator_user;
+
+\c rls_dev;
+-- schemaの利用権限
+GRANT USAGE ON SCHEMA public TO app_user;
+
+\c rls_test;
+-- schemaの利用権限
+GRANT USAGE ON SCHEMA public TO app_user;
